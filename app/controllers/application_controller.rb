5b875e89f0c78aed80afa746090fbd147d44f0ab
Blake <blake.doyle@hotmail.com.au>
2018-10-30 11:19:59 +1100

    Create custom registration field
    Co-authored-by: Tyson <technics652@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :add_devise_params, if: :devise_controller?
+
+    def add_devise_params
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name, :isSupplier?, :post_code, :avatar])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+    end
 end
