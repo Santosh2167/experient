b1d52cb19ade3569b5a2eed5b61c6365ed0355cd
Blake <blake.doyle@hotmail.com.au>
2018-10-30 15:10:18 +1100

    Add function to check if the user has an avatar and change text on profile page accordingly

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,12 @@
 class MainController < ApplicationController 
     def main
+    end
 
+    def profile
+        if current_user.avatar == nil
+            @hasavatar = "You do not have an avatar"
+        else
+            @hasavatar = "You do have an avatar"
+        end
     end
 end
