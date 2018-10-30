98bdf9a3b8dfba721301206a864beb8b3a1399b2
Tyson Hofstetter <tysonh652@gmail.com>
2018-10-30 14:05:09 +1100

    redirect to supplier sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,12 @@ class RegistrationsController < Devise::RegistrationsController
 
   def after_sign_up_path_for(resource)
     #  destroy_user_session_path, method: :delete
-    "https://www.google.com.au"
+    # "https://www.google.com.au"
+    new_supplier_registration_path
+  end
+
+  def new_supplier
+    build_resource({})
+    respond_with self.resource
   end
 end
