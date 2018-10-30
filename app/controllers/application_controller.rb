3a7693efcfead67ee075219632b2ce254c051ce1
Blake <blake.doyle@hotmail.com.au>
2018-10-30 11:23:48 +1100

    Ability to save custom registration fields
    Co-authored-by: Tyson <technics652@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     before_action :add_devise_params, if: :devise_controller?
 
     def add_devise_params
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name, :isSupplier?, :post_code, :avatar])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name, :isSupplier?, :post_code, :date_of_birth])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name,:last_name, :isSupplier?, :post_code, :date_of_birth])
     end
 end
