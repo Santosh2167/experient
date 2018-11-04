5cac1d9a4d6bbb4915c7902bf207d803fda9d87e
Blake <blake.doyle@hotmail.com.au>
2018-11-04 19:49:22 +1100

    try to fix transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -3,10 +3,11 @@ before_action :authenticate_user!
 before_action :add_params
     def new
         # @product = Product.find(params[:product_id])
-        @transaction = Transaction.create(params[:id])
+        @transaction = Transaction.new(add_params)
+        @transaction.save
     end
 
     def add_params
-        params.require().permit(:amount, :product_id, :user_id)
+        params.require(@transaction).permit(:amount, :product_id, :user_id)
     end
 end
