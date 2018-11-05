f377aaa05e217f994b3c1e3509c03fc87297dd5b
Blake <blake.doyle@hotmail.com.au>
2018-11-05 15:08:29 +1100

    add functional search bar to nav

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -86,7 +86,7 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
+ 
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
