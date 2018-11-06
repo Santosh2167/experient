12bab39a0bc1d20a17e94905f20004d3993d5ae7
Blake <blake.doyle@hotmail.com.au>
2018-11-07 10:43:57 +1100

    Add validation to user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :first_name, :last_name, :email, :password, :post_code, :date_of_birth, presence: true
+  validates :post_code, numericality: { only_integer: true }, length: { is: 4 }
   # has_many :interests, as: :categorable
 
   has_one :business
