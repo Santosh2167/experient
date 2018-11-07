bd36922831c6e067642c89a11217155ac1b2343d
Blake <blake.doyle@hotmail.com.au>
2018-11-07 14:43:04 +1100

    fix inability to add a business once user has been created

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,7 @@
 class Business < ApplicationRecord
  belongs_to :user
  validates :trading_name, :registered_business_name, :business_address, :post_code, :abn, :phone_number, presence: true
+ validates :post_code, numericality: { only_integer: true }, length: { is: 4 }
+ validates :abn, numericality: { only_integer: true }
+ validates :phone_number, numericality: { only_integer: true }
 end
