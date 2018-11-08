9e9f4975d427e2e749001bb752e583acb99c4cce
Blake <blake.doyle@hotmail.com.au>
2018-11-08 11:21:00 +1100

    Might use these

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :user
-
   belongs_to :age_group
   belongs_to :category
   has_many :transactions
   mount_uploader :image, ImageUploader
 
+  validates :name, :location, :description, :image, presence: true
+  validates :cost, presence: true, numericality: { only_integer: true }
   def self.search(search)
     if search && search!=""
         where(["name ILIKE ?","%#{search}%"]).or where(["category ILIKE ?","%#{search}%"]).or where(["location ILIKE ?","%#{search}%"]).or where(["description ILIKE ?","%#{search}%"])
